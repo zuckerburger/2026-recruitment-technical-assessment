f87467573842c3600e7c498fff01b125b1e758d9
zuckerburger <131454746+zuckerburger@users.noreply.github.com>
2026-02-17 15:08:26 +1100

    Create approach 1 and 2 for task1 python

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import List, Dict, Union
 from flask import Flask, request, jsonify
 import re
+import string
 
 # ==== Type Definitions, feel free to add or modify ===========================
 @dataclass
@@ -41,11 +42,39 @@ def parse():
 	return jsonify({'msg': parsed_name}), 200
 
 # [TASK 1] ====================================================================
-# Takes in a recipeName and returns it in a form that 
+# Takes in a recipeName and returns it in a form that satisfies conditions
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
 
+	# I used this built in approach since it's more readable and concise
+	recipeName = recipeName.replace('-', ' ').replace('_', ' ')
+	recipeName = re.sub(r'[^a-zA-Z\s]', '', recipeName)
+	recipeName = string.capwords(recipeName)
+	return recipeName if (recipeName) else None
+
+	# here's an alternative, more procedural approach, although less readable
+	# I used time it and this approach is around 10x faster since it doesn't need to make
+	# as many copies (since strings are immutable in python)
+	# pls hire me
+
+	# characters to be replaced by space - easily extendible
+	whitespace = {'-', '_'}
+	newRecipe = ""
+	# tracks last character added
+	prev = " "
+	for char in recipeName:
+		if char in whitespace or char.isspace():
+			# add a space iff last character isn't whitespace
+			if prev.isspace() == False:
+				newRecipe += " " 
+
+		elif char.isalpha():
+			# capitalise start of word, otherwise lower
+			char = char.upper() if prev.isspace() else char.lower()
+			newRecipe += char
+		prev = newRecipe[-1] if newRecipe else prev
+	return newRecipe if newRecipe else None
+
+	
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
